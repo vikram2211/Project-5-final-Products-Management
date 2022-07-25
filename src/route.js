5eeaedc445fa4096b914a9592468269ef9002221
vikram2211 <dk00668671@techmahindra.com>
2022-07-25 15:14:35 +0530

    get api

--- src/route.js
+++ src/route.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const midW = require("../middlewares/auth");
-const booksController = require("../controllers/booksController");
-const reviewsController = require("../controllers/reviewsController");
-const userController = require("../controllers/userController");
+// const midW = require("../middlewares/auth");
+// const booksController = require("../controllers/booksController");
+// const reviewsController = require("../controllers/reviewsController");
+const userController = require("../src/controller/userController");
 
 
-
+router.get("/user/:userId/profile",userController.getUserDetails);
 
 module.exports = router;
