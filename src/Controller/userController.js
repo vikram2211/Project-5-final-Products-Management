10be33225d39be416055f57467a865a4db2f21bc
vikram2211 <105534076+vikram2211@users.noreply.github.com>
2022-07-25 15:34:56 +0530

    Update userController.js

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -1,10 +1,11 @@
 const userModel = require("../model/userModel");
-const validator = require("../validator/validator")
+const validator = require("../validator/validator");
+const mongoose = require('mongoose');
 
 
 
 //=============================================User Register===============================================/
-const CreateUser = async function (req, res) {
+const createUser = async function (req, res) {
     try {
         if (!validator.isValidBody(body)) {
             return res.status(400).send({ status: false, msg: "User body should not be empty" });
@@ -57,3 +58,34 @@ const CreateUser = async function (req, res) {
         return res.status(500).send({ status: false, message: error.message })
     }
 };
+
+
+
+
+
+// -------------------------------------------Get User Details---------------------------------------------------//
+
+const getUserDetails = async function (req, res) {
+    try {
+        const user = req.params.userId;
+        if (validator.isValidObjectId(user)) {
+            res.status(400).send({ status: false, message: "Invalid UserId" })
+        }
+        if (!user) {
+            res.status(400).send({ status: false, message: "pz give userId in params" })
+        }
+
+        const findUser = await userModel.find({ _id: user }).select({ address: 1, _id: 1, fname: 1, lname: 1, email: 1, profileImage: 1, phone: 1, password: 1 });
+
+        if (findUser) {
+            res.status(200).send({ status: true, message: "User profile details", data: findUser })
+        }
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message });
+    }
+};
+
+
+module.exports = { createUser,
+                  getUserDetails
+                 };
